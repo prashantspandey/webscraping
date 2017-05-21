c7e606b52b039a7e7e83555ad940a29869968b4d
prashantspandey <prashants_pandey@outlook.com>
2017-05-21 21:44:43 +0530

    automatically chooses length of lists

--- multiprocessscraper.py
+++ multiprocessscraper.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from bs4 import BeautifulSoup
 import requests
 from  more_itertools import unique_everseen
@@ -6,15 +7,34 @@ import threading
 from multiprocessing.pool import ThreadPool
 import multiprocessing
 from multiprocessing import Pool
-
+from queue import Queue
 print('from here')
 
-with open('mensxplinks.pkl', 'rb') as links:
+with open('wikireddit.pkl', 'rb') as links:
     my_links = pickle.load(links)
 
 print('Number of links: ' + str(len(my_links)))
-list1 = my_links[:25]
-list2 = my_links[50:75]
+tot_links = len(my_links)
+a = int(tot_links /4)
+if not tot_links%4==0:
+    b =a
+    b = b+a
+    c =a+b
+    
+    d = tot_links - (3*a)
+    d = c+d
+elif tot_links % 4 ==0:
+    b =a
+    b = b+a
+    c =a+b
+print('first list - ' + str(a))
+print('second list - ' + str(b))
+print('third list - ' + str(c))
+print('fourth list - ' + str(d))
+list1 = my_links[:a]
+list2 = my_links[a:b]
+list3 = my_links[b:c]
+list4 = my_links[c:]
 wl = []
 wlt = []
 cont1 = []
@@ -22,8 +42,9 @@ cont2 = []
 wl2 = []
 wlt2 = []
 
+def get_data(my_links,q1):
 
-def get_data(my_links, working_links, working_linkstxt, cont,q1):
+
     for q, i in enumerate(my_links):
         try:
             page = requests.get(i)
@@ -32,36 +53,42 @@ def get_data(my_links, working_links, working_linkstxt, cont,q1):
         except:
             print('Couldnot open the page')
         try:
-            divs = soup.findAll('div', {'class': 'cont blk'})
-            sdivs = soup.findAll('span', {'itemprop': 'articleBody'})
-            tdivs = soup.findAll('div', {'itemprop': 'description'})
+            divs = soup.findAll('div', {'class': 'md'})
+            sdivs = soup.findAll('div', {'id': 'article-content'})
+            tdivs = soup.findAll('div', {'class': 'mw-content-ltr'})
             if divs:
                 for j in divs:
+                    print(str(q) + ' --- ' + str(i))
                     print('Article: ' + j.text.strip())
-                    cont.append(j.text.strip() + '\n\n||')
+                    #cont.append(j.text.strip() + '\n\n||')
+
                     q1.put(j.text.strip() + '\n\n||')
                     # working_links.append(i)
                     # working_linkstxt.append(i)
             elif sdivs:
 
                 for k in sdivs:
+                    print(str(q) + ' --- ' + str(i))
                     print('Article: ' + k.text.strip())
-                    cont.append(k.text.strip() + '\n\n||')
+                    #cont.append(k.text.strip() + '\n\n||')
+                    q1.put(k.text.strip() + '\n\n||')
                     # q1.put(k.text.strip() + '\n\n||')
                     # working_links.append(i)
                     # working_linkstxt.append(i)
             elif tdivs:
                 for l in tdivs:
+                    print(str(q) + ' --- ' + str(i))
                     print('Article: ' + l.text.strip())
-                    cont.append(l.text.strip() + '\n\n||')
+                    #cont.append(l.text.strip() + '\n\n||')
+                    q1.put(l.text.strip() + '\n\n||')
                     # q1.put(l.text.strip() + '\n\n||')
                     # working_links.append(i)
                     # working_linkstxt.append(i)
         except:
             print('soup not available')
-        # cont = list(unique_everseen(cont))
 
 
+zz =[]
 
 
 # p1 = multiprocessing.Process(target=get_data,args=(list1,wl,wlt,cont1,))
@@ -69,27 +96,75 @@ def get_data(my_links, working_links, working_linkstxt, cont,q1):
 
 # async_result1 = pool.apply_async(get_data, (list1,wl,wlt,cont1,divs1,sdivs1,tdivs1))
 # async_result2 = pool.apply_async(get_data, (list2,wl2,wlt2,cont2,divs2,sdivs2,tdivs2))
+def yield_from_processp1(q, p1):
+    while p1.is_alive():
+        p1.join(timeout=1)
+        while True:
+            try:
+                zz.append(q.get(block=False))
+            except:
+                break
+def yield_from_processp2(q, p2):
+    while p2.is_alive():
+        p2.join(timeout=1)
+        while True:
+            try:
+                zz.append(q.get(block=False))
+            except:
+                break
+def yield_from_processp3(q, p3):
+    while p3.is_alive():
+        p3.join(timeout=1)
+        while True:
+            try:
+                zz.append(q.get(block=False))
+            except:
+                break
+def yield_from_processp4(q, p4):
+    while p4.is_alive():
+        p4.join(timeout=1)
+        while True:
+            try:
+                zz.append(q.get(block=False))
+            except:
+                break
+
+def write_to_text_file(filename,arr):
+    filename = filename+'.txt'
+    filename = str(filename)
+    arr = list(unique_everseen(arr))
+    with open(filename, 'w', encoding="utf-8") as nd:
+        for p in arr:
+            nd.write('{}\n'.format(p))
 
 
 
 if __name__ == '__main__':
     # p = Pool()
-    # l1, l2 = p.map(get_data(my_links, wl, wlt, cont1))
+    # l1 = p.map(get_data(list1, wl, wlt, cont1))
     q1 = multiprocessing.Queue()
+
     q2 = multiprocessing.Queue()
-    p1 = multiprocessing.Process(target=get_data, args=(list1, wl, wlt, cont1,q1,))
-    p2 = multiprocessing.Process(target=get_data, args=(list2, wl2, wlt2, cont2,q2,))
+    p1 = multiprocessing.Process(target=get_data, args=(list1, q1,))
+    p2 = multiprocessing.Process(target=get_data, args=(list2,q1,))
+    p3 = multiprocessing.Process(target=get_data, args=(list3, q1,))
+    p4 = multiprocessing.Process(target=get_data, args=(list4, q1,))
     p1.start()
     p2.start()
-    p1.join()
-    p2.join()
-    print('i am here')
-    while q1.empty() is False:
-        print(q1.get())
+    p3.start()
+    p4.start()
+    yield_from_processp1(q1,p1)
+    yield_from_processp2(q1,p2)
+    yield_from_processp3(q1,p3)
+    yield_from_processp4(q1,p4)
+    write_to_text_file('wikiandreddit',zz)
 
-    # with open('mensxp.txt', 'w', encoding="utf-8") as nd:
-    #     for p in zz:
-    #         nd.write('{}\n'.format(p))
+
+
+
+
+
+  
 # with open('ndtvallpickle.pkl', 'wb') as ndtvpic:
 #     pickle.dump(cont, ndtvpic)
 # with open('ndtvworkinglinks.pkl', 'wb') as ndtvpi:
